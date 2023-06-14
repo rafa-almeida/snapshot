e0fb8a73f09ef38f260124ea9214036aa93a482a
rafa-almeida <rafasegt@gmail.com>
2023-06-13 23:55:25 -0300

    adicionando estruturas de pastas

---
+++ src/template/Default.js
@@ -0,0 +1,11 @@
+
+const Default = ({ children }) =>  {
+    return (
+        <>
+            <header>HEADER</header>
+            <footer>FOOTER</footer>
+        </>
+    )
+}
+
+export default Default
